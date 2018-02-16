25c558cd6a911213c9169350b9a5aa949f5f8057
Max Eremin <eremin_my@st.by>
2018-02-16 12:28:01 +0300

    added game logic

--- dist/app.js
+++ dist/app.js
@@ -1,3 +1,7 @@
+function _sliceIterator(arr, i) { var _arr = []; var _n = true; var _d = false; var _e = undefined; try { for (var _i = arr[Symbol.iterator](), _s; !(_n = (_s = _i.next()).done); _n = true) { _arr.push(_s.value); if (i && _arr.length === i) break; } } catch (err) { _d = true; _e = err; } finally { try { if (!_n && _i["return"] != null) _i["return"](); } finally { if (_d) throw _e; } } return _arr; }
+
+function _slicedToArray(arr, i) { if (Array.isArray(arr)) { return arr; } else if (Symbol.iterator in Object(arr)) { return _sliceIterator(arr, i); } else { throw new TypeError("Invalid attempt to destructure non-iterable instance"); } }
+
 var ui = {
   buildMenu: function buildMenu(context) {
     if (!context) {
@@ -23,7 +27,7 @@ var ui = {
     var boardHTML = "<div class=\"game-board\" data-size=".concat(size, ">");
 
     for (var idx = 0; idx < size * size; idx++) {
-      var rowIndex = idx < size ? 0 : Math.floor(idx / size);
+      var rowIndex = Math.floor(idx / size);
       var columnIndex = idx < size ? idx : idx % size;
       boardHTML += "<span class=\"game-cell\" \n                data-row=\"".concat(rowIndex, "\" data-column=\"").concat(columnIndex, "\"></span>");
     }
@@ -52,7 +56,7 @@ var Game = function Game(toolbarContext, gameContext) {
   this.gameContext = gameContext;
   this.players = [];
   this.moves = 0;
-  var player1 = new Player("Max", GAME_SHAPES.O);
+  var player1 = new Player("PDP11", GAME_SHAPES.O, null, true);
   var player2 = new Player("GoodKat", GAME_SHAPES.X);
   this.addPlayer(player1);
   this.addPlayer(player2);
@@ -71,25 +75,43 @@ var Game = function Game(toolbarContext, gameContext) {
     }
   });
   this.board = new Board(gameContext);
-  this.board.create(function (e) {
-    var player = _this.player;
-    var cell = $(e.target),
-        row = cell.data("row"),
-        column = cell.data("column");
-
-    _this.board.move(player.shape, row, column);
-
-    $(e.target).text(player.shape === GAME_SHAPES.X ? "X" : "O");
-    _this.moves++;
-
-    if (_this.moves >= 2 * _this.board.size - 1) {
-      var winner = _this.board.check();
-
-      if (winner) {
-        _this.board.stop(winner);
-      }
-    }
+  this.board.create(function (row, column, cell) {
+    return _this.nextMove(row, column, cell);
   });
+  this.currentPlayer = null;
+  this.move();
+};
+
+Game.prototype.move = function () {
+  var player = this.player;
+  this.currentPlayer = player;
+
+  if (player.isAI) {
+    var _player$calculateMove = player.calculateMove(this.board, this.moves),
+        _player$calculateMove2 = _slicedToArray(_player$calculateMove, 2),
+        row = _player$calculateMove2[0],
+        column = _player$calculateMove2[1];
+
+    var cell = this.board.boardContext.find(".game-cell[data-row=\"".concat(row, "\"][data-column=\"").concat(column, "\"]")).off("click");
+    this.nextMove(row, column, cell);
+  }
+};
+
+Game.prototype.nextMove = function (row, column, cell) {
+  var player = this.currentPlayer;
+  this.board.move(player.shape, row, column);
+  cell.text(player.shape === GAME_SHAPES.X ? "X" : "O");
+  this.moves++;
+
+  if (this.moves >= 2 * this.board.size - 1) {
+    var winner = this.board.check();
+
+    if (winner) {
+      return this.board.stop(winner);
+    }
+  }
+
+  return this.move();
 };
 
 Game.prototype.addPlayer = function (player) {
@@ -116,15 +138,67 @@ var Player = function Player(name, shape, color, isAI) {
   this.name = name;
   this.shape = shape;
   this.move = shape === GAME_SHAPES.X;
+  this.isAI = isAI;
+};
+
+Player.prototype.getRandomIndex = function (max) {
+  var min = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : 0;
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+};
+
+Player.prototype.getMove = function (gameBoard) {
+  var row = this.getRandomIndex(gameBoard.size - 1),
+      column = this.getRandomIndex(gameBoard.size - 1);
+
+  if (gameBoard.board[row][column] !== -1) {
+    var _getMove = this.getMove(gameBoard);
+
+    var _getMove2 = _slicedToArray(_getMove, 2);
+
+    row = _getMove2[0];
+    column = _getMove2[1];
+  }
+
+  return [row, column];
+};
+
+Player.prototype.calculateMove = function (gameBoard, moves) {
+  if (moves === 0) {
+    return [this.getRandomIndex(gameBoard.size - 1), this.getRandomIndex(gameBoard.size - 1)];
+  }
+
+  for (var i = 0; i < gameBoard.size; i++) {
+    for (var j = 0; j < gameBoard.size; j++) {
+      var board = gameBoard.copyBoard();
+
+      if (board[i][j] === -1) {
+        board[i][j] = this.shape;
+        var myWin = gameBoard.check(board);
+
+        if (myWin) {
+          return [i, j];
+        }
+
+        board[i][j] = this.shape === GAME_SHAPES.X ? GAME_SHAPES.O : GAME_SHAPES.X;
+        var enemyWin = gameBoard.check(board);
+
+        if (enemyWin) {
+          return [i, j];
+        }
+      }
+    }
+  }
+
+  return this.getMove(gameBoard);
 };
 
 var Board = function Board(gameContext) {
   var size = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : 3;
   this.gameContext = gameContext;
 
-  if (size < 2 || size > 5) {
+  if (size < 3 || size > 5) {
     size = 3;
-    console.warn("Для наиболее комфортной игры выберите значения размерности поля от 2 до 5. Поле size автоматически установлено в 3.");
+    console.warn("Для наиболее комфортной игры выберите значения размерности поля от 3 до 5. Поле size автоматически установлено в 3.");
   }
 
   ;
@@ -133,10 +207,29 @@ var Board = function Board(gameContext) {
   this.fill();
 };
 
+Board.prototype.copyBoard = function () {
+  var board = [];
+
+  for (var i = 0; i < this.board.length; i++) {
+    var row = [];
+
+    for (var j = 0; j < this.board[i].length; j++) {
+      row.push(this.board[i][j]);
+    }
+
+    board.push(row);
+  }
+
+  return board;
+};
+
 Board.prototype.create = function (move) {
   this.boardContext = ui.buildBoard(this.gameContext, this.size);
   this.boardContext.find(".game-cell").one("click", function (e) {
-    return move(e);
+    var cell = $(e.target),
+        row = cell.data("row"),
+        column = cell.data("column");
+    move(row, column, cell);
   });
 };
 
@@ -153,42 +246,44 @@ Board.prototype.fill = function () {
 };
 
 Board.prototype.check = function () {
-  var result = null;
+  var board = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : null;
+  board = board ? board : this.board;
+  var result = null,
+      emptyCellsCount = 0;
   var lastIndex = this.size - 1;
 
   for (var i = 0; i < this.size; i++) {
-    var x = this.board[i][i];
-
-    if (x === -1) {
-      continue;
-    }
-
+    var x = board[i][i];
     var X = true,
         Y = true,
         D = i === 0,
         Db = i === 0;
 
     for (var j = 0; j < this.size; j++) {
-      if (this.board[i][j] !== x) {
+      if (board[i][j] === -1) {
+        emptyCellsCount++;
+      }
+
+      if (board[i][j] === -1 || board[i][j] !== x) {
         X = false;
       }
 
-      if (this.board[j][i] !== x) {
+      if (board[i][j] === -1 || board[j][i] !== x) {
         Y = false;
       }
 
-      if (i === 0 && this.board[j][j] !== x) {
+      if (i === 0 && (board[j][j] === -1 || board[j][j] !== x)) {
         D = false;
       }
 
-      if (i === 0 && this.board[lastIndex - j][j] !== this.board[lastIndex][0]) {
+      if (i === 0 && (board[lastIndex - j][j] === -1 || board[lastIndex - j][j] !== board[lastIndex][0])) {
         Db = false;
       }
     }
 
     if (X || Y || D || Db) {
-      var type = void 0;
-      var player = x;
+      var type = void 0,
+          player = x;
 
       if (X) {
         type = WIN_TYPES.ROW;
@@ -198,7 +293,7 @@ Board.prototype.check = function () {
         type = WIN_TYPES.MAIN_DIAGONAL;
       } else if (Db) {
         type = WIN_TYPES.DIAGONAL;
-        player = this.board[lastIndex][0];
+        player = board[lastIndex][0];
       }
 
       result = {
@@ -210,7 +305,9 @@ Board.prototype.check = function () {
     }
   }
 
-  return result;
+  return result ? result : emptyCellsCount ? result : {
+    player: -1
+  };
 };
 
 Board.prototype.move = function (shape, row, column) {
@@ -218,16 +315,20 @@ Board.prototype.move = function (shape, row, column) {
 };
 
 Board.prototype.stop = function (winner) {
-  this.boardContext.find(".game-cell").off("click");
-  console.log(winner);
-  this.boardContext.find(".game-cell").each(function (index, cell) {
+  var _this3 = this;
+
+  if (winner.player === -1) {
+    return;
+  }
+
+  this.boardContext.find(".game-cell").off("click").each(function (index, cell) {
     cell = $(cell);
 
     switch (winner.type) {
       case WIN_TYPES.COLUMN:
         {
           if (cell.data("column") === winner.index) {
-            $(cell).addClass("game-cell_won");
+            cell.addClass("game-cell_won");
           }
 
           break;
@@ -236,7 +337,7 @@ Board.prototype.stop = function (winner) {
       case WIN_TYPES.ROW:
         {
           if (cell.data("row") === winner.index) {
-            $(cell).addClass("game-cell_won");
+            cell.addClass("game-cell_won");
           }
 
           break;
@@ -245,7 +346,7 @@ Board.prototype.stop = function (winner) {
       case WIN_TYPES.MAIN_DIAGONAL:
         {
           if (cell.data("row") === cell.data("column")) {
-            $(cell).addClass("game-cell_won");
+            cell.addClass("game-cell_won");
           }
 
           break;
@@ -253,6 +354,12 @@ Board.prototype.stop = function (winner) {
 
       case WIN_TYPES.DIAGONAL:
         {
+          var rowIndex = Math.floor(index / _this3.size);
+
+          if (cell.data("row") === rowIndex && cell.data("column") === _this3.size - 1 - rowIndex) {
+            cell.addClass("game-cell_won");
+          }
+
           break;
         }
     }
--- src/app.js
+++ src/app.js
@@ -23,9 +23,7 @@ const ui = {
         if(!context) { return; }
         let boardHTML = `<div class="game-board" data-size=${size}>`;
         for (let idx = 0; idx < size * size; idx++) {
-            const rowIndex = idx < size
-                ? 0
-                : Math.floor(idx / size);
+            const rowIndex = Math.floor(idx / size);
             const columnIndex = idx < size
                 ? idx
                 : idx % size;
@@ -56,7 +54,7 @@ const Game = function(toolbarContext, gameContext) {
     this.players = [];
     this.moves = 0;
 
-    const player1 = new Player("Max", GAME_SHAPES.O);
+    const player1 = new Player("PDP11", GAME_SHAPES.O, null, true);
     const player2 = new Player("GoodKat", GAME_SHAPES.X);
 
     this.addPlayer(player1);
@@ -75,23 +73,33 @@ const Game = function(toolbarContext, gameContext) {
     });
 
     this.board = new Board(gameContext);
+    this.board.create((row, column, cell) => this.nextMove(row, column, cell));
 
-    this.board.create((e) => {
-        const player = this.player;
-        const cell = $(e.target),
-            row = cell.data("row"),
-            column = cell.data("column");
-        this.board.move(player.shape, row, column);
-        $(e.target).text(player.shape === GAME_SHAPES.X ? "X" : "O");
-        this.moves++;
-        
-        if(this.moves >= (2 * this.board.size - 1)) {
-            const winner = this.board.check();
-            if(winner) {
-                this.board.stop(winner);
-            }
-        }
-    });
+    this.currentPlayer = null;
+    this.move();
+};
+Game.prototype.move = function() {
+    const player = this.player;
+    this.currentPlayer = player;
+    if(player.isAI) {
+        const [ row, column ] = player.calculateMove(this.board, this.moves);
+        const cell = this.board.boardContext.find(
+            `.game-cell[data-row="${row}"][data-column="${column}"]`
+        ).off("click");
+        this.nextMove(row, column, cell);
+    }
+};
+Game.prototype.nextMove = function(row, column, cell) {
+    const player = this.currentPlayer;
+    this.board.move(player.shape, row, column);
+    cell.text(player.shape === GAME_SHAPES.X ? "X" : "O");
+    this.moves++;
+    
+    if(this.moves >= (2 * this.board.size - 1)) {
+        const winner = this.board.check();
+        if(winner) { return this.board.stop(winner); }
+    }
+    return this.move();
 };
 Game.prototype.addPlayer = function(player) {
     if(player instanceof Player) {
@@ -115,26 +123,74 @@ const Player = function (name, shape, color, isAI) {
     this.name = name;
     this.shape = shape;
     this.move = shape === GAME_SHAPES.X;
+    this.isAI = isAI;
+};
+Player.prototype.getRandomIndex = (max, min = 0) => {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+};
+Player.prototype.getMove = function(gameBoard) {
+    let row = this.getRandomIndex(gameBoard.size - 1),
+        column = this.getRandomIndex(gameBoard.size - 1);
+    if(gameBoard.board[row][column] !== -1) {
+        [ row, column ] = this.getMove(gameBoard);
+    }
+    return [ row, column ];
+};
+Player.prototype.calculateMove = function(gameBoard, moves) {   
+    if(moves === 0) {
+        return [ 
+            this.getRandomIndex(gameBoard.size - 1), this.getRandomIndex(gameBoard.size - 1) 
+        ];
+    }
+    for (let i = 0; i < gameBoard.size; i++) {
+        for (let j = 0; j < gameBoard.size; j++) {
+            const board = gameBoard.copyBoard();
+            if(board[i][j] === -1) {
+                board[i][j] = this.shape;
+                const myWin = gameBoard.check(board);
+                if(myWin) { return [ i, j ]; }
+                board[i][j] = this.shape === GAME_SHAPES.X 
+                    ? GAME_SHAPES.O 
+                    : GAME_SHAPES.X;
+                const enemyWin = gameBoard.check(board);
+                if(enemyWin) { return [ i, j ]; }
+            }
+        }
+    }
+    return this.getMove(gameBoard);
 };
 
 const Board = function(gameContext, size = 3) {
-
     this.gameContext = gameContext;
-    if(size < 2 || size > 5) {
+    if(size < 3 || size > 5) {
         size = 3;
         console.warn(
-            "Для наиболее комфортной игры выберите значения размерности поля от 2 до 5. Поле size автоматически установлено в 3."
+            "Для наиболее комфортной игры выберите значения размерности поля от 3 до 5. Поле size автоматически установлено в 3."
         );
     };
     this.size = size;
     this.board = [];
-   
     this.fill();
-
+};
+Board.prototype.copyBoard = function() {
+    const board = [];
+    for (let i = 0; i < this.board.length; i++) {
+        const row = [];
+        for (let j = 0; j < this.board[i].length; j++) {
+            row.push(this.board[i][j]);
+        }
+        board.push(row);
+    }
+    return board;
 };
 Board.prototype.create = function(move) {
     this.boardContext = ui.buildBoard(this.gameContext, this.size);
-    this.boardContext.find(".game-cell").one("click", e => move(e));
+    this.boardContext.find(".game-cell").one("click", e => {
+        const cell = $(e.target),
+            row = cell.data("row"),
+            column = cell.data("column");
+        move(row, column, cell);
+    });
 };
 Board.prototype.fill = function() {
     for (let i = 0; i < this.size; i++) {
@@ -145,24 +201,28 @@ Board.prototype.fill = function() {
         this.board.push(row);
     }
 };
-Board.prototype.check = function() {
-    let result = null;
+Board.prototype.check = function(board = null) {
+    board = board ? board : this.board;
+    let result = null, emptyCellsCount = 0;
     const lastIndex = this.size - 1;
     for (let i = 0; i < this.size; i++) {
-        const x = this.board[i][i];
-        if(x === -1) { continue; }
+        const x = board[i][i];
         let X = true, Y = true, D = i === 0, Db = i === 0;
         for (let j = 0; j < this.size; j++) {
-            if(this.board[i][j] !== x) { X = false; }
-            if(this.board[j][i] !== x) { Y = false; }
-            if(i === 0 && this.board[j][j] !== x) { D = false; } 
+            if(board[i][j] === -1) { emptyCellsCount++; }
+            if(board[i][j] === -1 || board[i][j] !== x) { X = false; }
+            if(board[i][j] === -1 || board[j][i] !== x) { Y = false; }
+            if(i === 0 
+                && (board[j][j] === -1
+                || board[j][j] !== x)
+            ) { D = false; } 
             if(i === 0
-                && this.board[lastIndex - j][j] !== this.board[lastIndex][0]
+                && (board[lastIndex - j][j] === -1
+                || board[lastIndex - j][j] !== board[lastIndex][0])
             ) { Db = false; } 
         }
         if(X || Y || D || Db) {
-            let type;
-            let player = x;
+            let type, player = x;
             if(X) {
                 type = WIN_TYPES.ROW;
             } else if(Y) {
@@ -171,52 +231,52 @@ Board.prototype.check = function() {
                 type = WIN_TYPES.MAIN_DIAGONAL;
             } else if(Db) {
                 type = WIN_TYPES.DIAGONAL;
-                player = this.board[lastIndex][0];
+                player = board[lastIndex][0];
             }
-            result = { 
-                player,
-                index: i, 
-                type
-            };
+            result = { player, index: i, type };
             break;
         }
     }
-    return result;
+    return result ? result : emptyCellsCount ? result : { player: -1 };
 };
 Board.prototype.move = function(shape, row, column) {
     this.board[row][column] = shape;
 }
 Board.prototype.stop = function(winner) {
-    this.boardContext.find(".game-cell").off("click");
-
-    console.log(winner);
-    this.boardContext.find(".game-cell").each((index, cell) => {
-        cell = $(cell);
-        switch (winner.type) {
-            case WIN_TYPES.COLUMN: {
-                if(cell.data("column") === winner.index) {
-                    $(cell).addClass("game-cell_won");
+    if(winner.player === -1) { return; }
+    this.boardContext.find(".game-cell").off("click")
+        .each((index, cell) => {
+            cell = $(cell);
+            switch (winner.type) {
+                case WIN_TYPES.COLUMN: {
+                    if(cell.data("column") === winner.index) {
+                        cell.addClass("game-cell_won");
+                    }
+                    break;
                 }
-                break;
-            }
-            case WIN_TYPES.ROW: {
-                if(cell.data("row") === winner.index) {
-                    $(cell).addClass("game-cell_won");
+                case WIN_TYPES.ROW: {
+                    if(cell.data("row") === winner.index) {
+                        cell.addClass("game-cell_won");
+                    }
+                    break;
                 }
-                break;
-            }
-            case WIN_TYPES.MAIN_DIAGONAL: {
-                if(cell.data("row") === cell.data("column")) {
-                    $(cell).addClass("game-cell_won");
+                case WIN_TYPES.MAIN_DIAGONAL: {
+                    if(cell.data("row") === cell.data("column")) {
+                        cell.addClass("game-cell_won");
+                    }
+                    break;
                 }
-                break;
-            }
-            case WIN_TYPES.DIAGONAL: {
-                break;
-            }
-        };
-    });
-    
+                case WIN_TYPES.DIAGONAL: {
+                    const rowIndex = Math.floor(index / this.size);
+                    if(cell.data("row") === rowIndex
+                        && cell.data("column") === (this.size - 1 - rowIndex)
+                    ) {
+                        cell.addClass("game-cell_won");  
+                    }
+                    break;
+                }
+            };
+        });
 };
 
 {
